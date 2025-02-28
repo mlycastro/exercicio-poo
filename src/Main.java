b2b412e627f5635bf506d8a388efda16bbfac6bc
mlycastro <emillycastrotrabalho@gmail.com>
2025-02-27 22:04:03 -0300

    par ou impar

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if (num % 2 == 0){
+            System.out.println("o numero é par");
+        } else {
+            System.out.println("o numero é impar");
+        }
+
+    }
+}
