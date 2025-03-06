66c5d9ff221791065fc25e220ca3c988be2c1c35
mlycastro <emillycastrotrabalho@gmail.com>
2025-03-06 19:18:51 -0300

    cpar ou impar

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
+import static java.lang.System.*;
+
 public class Main {
     public static void main(String[] args) {
         int num = 10;
 
         if (num % 2 == 0){
-            System.out.println("o numero é par");
+            out.println("o numero é par");
         } else {
-            System.out.println("o numero é impar");
+            out.println("o numero é impar");
         }
 
     }
