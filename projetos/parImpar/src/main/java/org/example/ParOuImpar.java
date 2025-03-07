ad222fc4f1b1b67a3dba5e468bffbf363f52b334
unknown <emillycastrotrabalho@gmail.com>
2025-03-06 21:40:56 -0300

    par ou impar

---
+++ projetos/parImpar/src/main/java/org/example/ParOuImpar.java
@@ -0,0 +1,15 @@
+public class ParOuImpar {
+    public static boolean ehPar(int num) {
+        return num % 2 == 0;
+    }
+    public static void main(String[]args) {
+        int num = 10;
+        System.out.println(ehPar(num));
+
+        num = 11;
+                System.out.println(ehPar(num));
+
+
+        }
+
+    }
